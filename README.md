509ae30a3d2d991c3c7fbb71a6b5d4b30e6d1720
darshan45672 <drshnbhandary@gmail.com>
2023-09-16 08:29:08 +0530

    done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Bakery
+# Online-Bakery-Shop-Management-System-Php
+This system allows bakery owners to sell their products to customer online via a web interface. The user can order and then pay for the item.
---
+++ includes/footer.php
@@ -0,0 +1,212 @@
+<!--Bootstrap -->
+
+<link rel="stylesheet" href="assets2/css/style.css" type="text/css">
+<link rel="stylesheet" href="assets2/css/owl.carousel.css" type="text/css">
+<link rel="stylesheet" href="assets2/css/owl.transitions.css" type="text/css">
+<link href="assets2/css/slick.css" rel="stylesheet">
+<link href="assets2/css/bootstrap-slider.min.css" rel="stylesheet">
+<link href="assets2/css/font-awesome.min.css" rel="stylesheet">
+<?php
+if(isset($_POST['emailsubscibe']))
+{
+$subscriberemail=$_POST['subscriberemail'];
+$sql ="SELECT SubscriberEmail FROM pato_subscribers WHERE SubscriberEmail=:subscriberemail";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':subscriberemail', $subscriberemail, PDO::PARAM_STR);
+$query-> execute();
+$results = $query -> fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query -> rowCount() > 0)
+{
+echo "<script>alert('Already Subscribed.');</script>";
+}
+else{
+$sql="INSERT INTO  pato_subscribers(SubscriberEmail) VALUES(:subscriberemail)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':subscriberemail',$subscriberemail,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+echo "<script>alert('Subscribed successfully.');</script>";
+}
+else
+{
+echo "<script>alert('Something went wrong. Please try again');</script>";
+}
+}
+}
+?>
+
+<!-- <footer> -->
+<footer>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Footer Design | Hemant</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="style.css" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+    />
+
+    <style>
+      body {
+  line-height: 1.5;
+  font-family: "Poppins", sans-serif;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1170px;
+  margin: auto;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+ul {
+  list-style: none;
+}
+
+.footer {
+  background-color: #24262b;
+  padding: 70px 0;
+}
+
+.footer-col {
+  width: 25%;
+  padding: 0 15px;
+}
+
+.footer-col h4 {
+  font-size: 18px;
+  color: #ffffff;
+  text-transform: capitalize;
+  margin-bottom: 35px;
+  font-weight: 500;
+  position: relative;
+}
+
+.footer-col h4::before {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: -10px;
+  background-color: #e91e63;
+  height: 2px;
+  box-sizing: border-box;
+  width: 50px;
+}
+
+.footer-col ul li:not(:last-child) {
+  margin-bottom: 10px;
+}
+
+.footer-col ul li a {
+  font-size: 16px;
+  text-transform: capitalize;
+  color: #ffffff;
+  text-decoration: none;
+  font-weight: 300;
+  color: #bbbbbb;
+  display: block;
+  transition: all 0.3s ease;
+}
+
+.footer-col ul li a:hover {
+  color: #ffffff;
+  padding-left: 8px;
+}
+
+.footer-col .social-links a {
+  display: inline-block;
+  height: 40px;
+  width: 40px;
+  background-color: rgba(255, 255, 255, 0.2);
+  margin: 0 10px 10px 0;
+  text-align: center;
+  line-height: 40px;
+  border-radius: 50%;
+  color: #ffffff;
+  transition: all 0.5s ease;
+}
+
+.footer-col .social-links a:hover {
+  color: #24262b;
+  background-color: #ffffff;
+}
+
+
+@media (max-width: 767px) {
+  .footer-col {
+    width: 50%;
+    margin-bottom: 30px;
+  }
+}
+
+
+@media (max-width: 574px) {
+  .footer-col {
+    width: 100%;
+  }
+}
+    </style>
+  </head>
+  <body>
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="footer-col">
+            <h4>company</h4>
+            <ul>
+              <li><a href="#">about us</a></li>
+              <li><a href="#">our services</a></li>
+              <li><a href="#">privacy policy</a></li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>get help</h4>
+            <ul>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">shipping</a></li>
+              <li><a href="#">returns</a></li>
+              <li><a href="#">order status</a></li>
+              <li><a href="#">payment options</a></li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>Cake shop</h4>
+            <ul>
+              <li><a href="#">Birthday</a></li>
+              <li><a href="#">Aniversary</a></li>
+              <li><a href="#">Marriage</a></li>
+              <li><a href="#">Party</a></li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>follow us</h4>
+            <div class="social-links">
+              <a href="#"><i class="fab fa-facebook-f"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
+              <a href="#"><i class="fab fa-instagram"></i></a>
+              <a href="#"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
+</footer>
+<!-- </footer> -->
